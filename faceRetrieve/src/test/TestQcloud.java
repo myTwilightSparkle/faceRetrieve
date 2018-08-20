a48f366964eac2e0230041b5501c2399156a70ec
myTwilightSparkle <2570945863@qq.com>
2018-08-20 12:21:12 +0800

    修改了test

--- faceRetrieve/src/test/TestQcloud.java
+++ faceRetrieve/src/test/TestQcloud.java
@@ -13,6 +13,7 @@ import com.alibaba.fastjson.JSON;
 import com.qcloud.image.ImageClient;
 import com.qcloud.image.request.FaceAddFaceRequest;
 import com.qcloud.image.request.FaceAddGroupIdsRequest;
+import com.qcloud.image.request.FaceDelPersonRequest;
 import com.qcloud.image.request.FaceDetectRequest;
 import com.qcloud.image.request.FaceIdentifyRequest;
 import com.qcloud.image.request.FaceNewPersonRequest;
@@ -57,14 +58,14 @@ public class TestQcloud {
 	
 	@Test
 	public void faceDetect(){
-		//url隸ｷ豎よ婿�ｿｽ?
+		//url请求方式
 		//String imageUrl = "http://i3.sinaimg.cn/ty/g/2013-11-30/U5244P6T12D6910497F44DT20131130112919.jpg";
 		String imageUrl = "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1534480544241&di=99d764b745d26ad1bdc5e841a585e9d7&imgtype=0&src=http%3A%2F%2Fww2.sinaimg.cn%2Fbmiddle%2F64143be5jw1dmj4ce3ki9j.jpg";
 		int mode = 0;//0譏ｯ�ｿｽ?譛我ｺｺ閼ｸ?�ｿｽ?1譏ｯ�ｿｽ?螟ｧ莠ｺ閼ｸ
 		String result1 = imageClient.faceDetect(new FaceDetectRequest(bucketName, imageUrl, mode));
 		System.out.println(result1);
 		
-		//image隸ｷ豎よ婿�ｿｽ?
+		//image请求方式
 		File image = new File("D:/images/img4.jpg");
 		String name = image.getName();
 		FaceDetectRequest faceDetectRequest = new FaceDetectRequest(bucketName, name, image, 0);
@@ -73,30 +74,40 @@ public class TestQcloud {
 	}
 	
 	/*
-	譁ｰ蠅樔ｺｺ
+	 * 添加人
 	 */
 	@Test
 	public void addPerson(){
 		String[] groupids = new String[1];
 		groupids[0] = groupid;
-		image = new File("D:/images/messi.jpg");
+		image = new File("assert/messi1.jpg");
 		FaceNewPersonRequest faceNewPersonRequest = new FaceNewPersonRequest(bucketName, personid, groupids, image, "messi", "footballplayer");
 		String result = imageClient.faceNewPerson(faceNewPersonRequest);
-		System.out.println("譁ｰ蠅樔ｺｺ?�ｿｽ? "+result);
+		System.out.println("添加人: "+result);
 	}
 	
 	/*
-	謚贋ｺｺ豺ｻ�ｿｽ?蛻ｰ�ｿｽ?
+	 * 删除人
+	 */
+	@Test
+	public void deletPerson() {
+		FaceDelPersonRequest request = new FaceDelPersonRequest(bucketName, personid);
+		String result = imageClient.faceDelPerson(request );
+		System.out.println("删除人: "+result);
+	}
+	
+	/*
+	 * 添加组
 	 */
 	@Test
 	public void personSetGroup(){
 		FaceAddGroupIdsRequest faceAddGroupIdsRequest = new FaceAddGroupIdsRequest(bucketName, personid, groupid);
 		String result = imageClient.faceAddGroupIds(faceAddGroupIdsRequest, false);
-		System.out.println("豺ｻ�ｿｽ?蛻ｰ�ｿｽ?�ｿｽ? "+result);
+		System.out.println("添加组: "+result);
 	}
 	
 	/*
-	譁ｰ蠅樔ｺｺ閼ｸ
+	 * 添加脸
 	 */
 	@Test
 	public void addFace(){
@@ -104,23 +115,23 @@ public class TestQcloud {
 		addFaceImageList[0] = image;
 		FaceAddFaceRequest faceAddFaceRequest = new FaceAddFaceRequest(bucketName, addFaceImageList, personid, "");
 		String result = imageClient.faceAddFace(faceAddFaceRequest);
-		System.out.println("譁ｰ蠅樔ｺｺ閼ｸ?�ｿｽ? "+result);
+		System.out.println("添加脸: "+result);
 	}
 	
 	/*
-	莠ｺ閼ｸ�ｿｽ?邏｢
+	 * 识别
 	 */
 	@Test
 	public void faceIdentify(){
-		//逵滂ｿｽ?隘ｿconfidence100
-		image = new File("D:/images/img4.jpg");
-		//莨頑恬�ｿｽ?隘ｿconfidence75
-		//image = new File("D:/images/iranmessi.png");
-		//鮴壽￡荵議onfidence23
-		//image = new File("D:/images/img2.jpg");
+		// 梅西自己的另一张照片 candidate:messi confidence:100
+		//image = new File("assert/messi2.jpg");
+		// 伊朗梅西 candidate:messi confidence:96
+		//image = new File("assert/iranmessi.jpg");
+		// 龚恒之 candidate:messi confidence:10
+		image = new File("assert/morte.jpg");
 		FaceIdentifyRequest faceIdentifyRequest = new FaceIdentifyRequest(bucketName, groupid, image);
 		String result = imageClient.faceIdentify(faceIdentifyRequest);
-		System.out.println("莠ｺ閼ｸ�ｿｽ?邏｢?�ｿｽ? "+result);
+		System.out.println("检索结果: "+result);
 	}
 	
 	@Test
